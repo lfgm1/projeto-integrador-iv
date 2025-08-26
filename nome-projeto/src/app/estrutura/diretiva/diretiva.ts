bb0c9ecd38de0fcf7ce46561f1978f230e9854f2
Luiz Felipe Gomes Magalhães <lowwer44@hotmail.com>
2025-08-26 23:28:48 +0000

    Exercicio Projeto Integrador

--- nome-projeto/src/app/estrutura/diretiva/diretiva.ts
+++ nome-projeto/src/app/estrutura/diretiva/diretiva.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-diretiva',
-  standalone: false,
-  templateUrl: './diretiva.html',
-  styleUrl: './diretiva.css'
+selector: 'app-diretiva',
+standalone: false,
+templateUrl: './diretiva.html',
+styleUrl: './diretiva.css'
 })
 export class Diretiva {
+conteudo = false;
 
-  cursos = [
-    { nome: 'Luiz', id: '1', status: 'ativo' },
-    { nome: 'Luiz2', id: '2', status: 'Negativo' }
-  ];
+mostrarOcultar() {
+this.conteudo = !this.conteudo;
+}
 
+listaItens = [
+{id: '001', nome: 'Gabriel', ativo: true},
+{id: '002', nome: 'Volpe', ativo: false},
+{id: '003', nome: 'Luiz', ativo: true},
+{id: '004', nome: 'Possarle', ativo: false}
+];
+
+tamanhoFonte = 15;
+
+aumentarFonte() {
+this.tamanhoFonte = this.tamanhoFonte + 1;
+}
+
+diminuirFonte(){
+this.tamanhoFonte = this.tamanhoFonte - 1;
+}
 }
